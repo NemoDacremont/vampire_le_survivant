76b90cc8617188e0954db270891e87563347781c
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-20 09:53:49 +0100

    Add enemy init function

---
+++ src/enemies/enemy.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+static var _target: Node2D
+
+
+func init(target: Node2D):
+	_target = target
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
