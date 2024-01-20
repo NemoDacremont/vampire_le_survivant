0beab5b834e6f2570462ec62e90fb4f02e9449e4
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-20 10:15:59 +0100

    Add signal on_death and on_hit, add dmg handling and fix a typo

--- src/enemies/enemy.gd
+++ src/enemies/enemy.gd
@@ -1,21 +1,30 @@
 extends Node2D
 
-const ENEMY_DEFAULT_VELOCITY: float = 2 * 48;
+signal on_death
+signal on_hit(dmg: int)
+
+
 
 static var _target: Node2D
+
+const ENEMY_DEFAULT_VELOCITY: float = 2 * 48;
 static var _movement_direction: Vector2
 static var _velocity: Vector2
+
+const ENEMY_DEFAULT_MAX_HP = 3
+static var _current_hp = ENEMY_DEFAULT_MAX_HP
 
 
 func init(target: Node2D) -> void:
 	_target = target
+	_current_hp = ENEMY_DEFAULT_MAX_HP
 
 
 func _physics_process(delta) -> void:
 	_movement_direction = _target.position - position
 	_movement_direction = _movement_direction.normalized()
 
-	_velocity = ENEMY_VELOCITY * _movement_direction
+	_velocity = ENEMY_DEFAULT_VELOCITY * _movement_direction
 
 	position += _velocity * delta
 
@@ -23,3 +32,13 @@ func _physics_process(delta) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
 	pass
+
+
+func _on_attack_hitbox_area_entered(_area: Area2D):
+	_current_hp -= 1;
+
+	emit_signal("on_hit", 1)
+
+	if (_current_hp <= 0):
+		emit_signal("on_death")
+
