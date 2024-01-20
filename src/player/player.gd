14fa142d7b91bdbd3afc3ac1a0360e646f9ecca4
Gatien Menaud <gatienmenaud@gmail.com>
2024-01-20 17:37:22 +0100

    player sprite, collision and movements

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+var velocity: Vector2
+var direction: Vector2
+@export var speed: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2(0, 0)
+	direction = Vector2(0, 0)
+	speed = 500
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	set_direction()
+	velocity = direction.normalized() * speed
+	self.position += velocity * delta
+	
+func set_direction() -> void:
+	direction = Vector2(Input.is_action_pressed("ui_right"), Input.is_action_pressed("ui_down"))\
+	- Vector2(Input.is_action_pressed("ui_left"), Input.is_action_pressed("ui_up"))
