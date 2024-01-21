02fb4c12953465ff4f0ca617da0089dee2a6e342
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-21 09:48:10 +0100

    Add first attack file

--- src/player/player.gd
+++ src/player/player.gd
@@ -18,9 +18,10 @@ func _ready():
 func _physics_process(delta):
 	set_direction()
 	velocity = direction.normalized() * speed
+
 	self.position += velocity * delta
 
 func set_direction() -> void:
 	direction = Vector2(Input.is_action_pressed("ui_right"), Input.is_action_pressed("ui_down")) \
-	- Vector2(Input.is_action_pressed("ui_left"), Input.is_action_pressed("ui_up"))
+		- Vector2(Input.is_action_pressed("ui_left"), Input.is_action_pressed("ui_up"))
 
