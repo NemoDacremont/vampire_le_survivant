75b24c75192705e34919af74d2190b7a53a9c758
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-21 11:47:32 +0100

    add health_lost and death signals to health_component

--- src/components/health_component/health_component.gd
+++ src/components/health_component/health_component.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name HealthComponent
 
+signal health_lost(new_hp: float)
+signal death
+
 @export var MAX_HEALTH: float = 3.0
 var _hp: float = MAX_HEALTH
 
@@ -14,9 +17,14 @@ func get_hps() -> float:
 
 
 # Returns hps after applying dmg
-func deal_dmg(dmgs: float) -> float:
+func recieve_dmg(dmgs: float) -> float:
 	_hp -= dmgs
 
+	if get_hps() <= 0:
+		emit_signal("death")
+		return 0
+
+	emit_signal("health_lost", get_hps())
 	return get_hps()
 
 
