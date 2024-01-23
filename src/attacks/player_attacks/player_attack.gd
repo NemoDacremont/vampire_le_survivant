d81521d75af2417c478188858cf890cca04e719d
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-23 08:51:54 +0100

    Create a player_attack scene

---
+++ src/attacks/player_attacks/player_attack.gd
@@ -0,0 +1,10 @@
+extends Node2D
+class_name PlayerAttack
+
+
+@export var collision_component: CollisionComponent
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
