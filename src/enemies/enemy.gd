c4d37d534a4a6a6b277a9e1a560bed3cb1ca5308
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-01-26 23:23:22 +0100

    Add Collisions to enemies and pleyr

--- src/enemies/enemy.gd
+++ src/enemies/enemy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 class_name Enemy
 
 signal death
@@ -9,20 +9,20 @@ static var _target: Node2D
 
 const ENEMY_DEFAULT_VELOCITY: float = 2 * 48;
 var _movement_direction: Vector2
-var _velocity: Vector2
+# var _velocity: Vector2
 
 
 func init(target: Node2D) -> void:
 	_target = target
 
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	_movement_direction = _target.position - position
 	_movement_direction = _movement_direction.normalized()
 
-	_velocity = ENEMY_DEFAULT_VELOCITY * _movement_direction
+	velocity = ENEMY_DEFAULT_VELOCITY * _movement_direction
 
-	position += _velocity * delta
+	move_and_collide(velocity * _delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
