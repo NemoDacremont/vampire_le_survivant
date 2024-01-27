184e0f491a056a5a827b236c2cdca9b5ff841867
Theo-Monseran <tmonseran@enseirb-matmeca.fr>
2024-01-27 19:04:36 +0100

    Added segway + fixed shotgun

--- src/attacks/player_attacks/fireball/shotgun_bullet/shotgun_bullet.gd
+++ src/attacks/player_attacks/fireball/shotgun_bullet/shotgun_bullet.gd
@@ -3,11 +3,22 @@ extends Fireball
 @onready var sprite_node: Sprite2D = $Sprite
 @onready var collision_shape: CollisionShape2D = $CollisionComponent/CollisionShape2D
 
+
+func _ready():
+	var theta : float = atan2(_direction.y, _direction.x)
+	sprite_node.rotation = -theta
+	collision_shape.rotation = -theta
+
+
+func init(spawn_position, direction, speed, max_distance = 1000.0):
+	super(spawn_position, direction, speed, max_distance)
+
+
 func _physics_process(delta):
 	_velocity = _speed * _direction * 0.5
 
 	position += delta * _velocity
-	_distance += delta * _speed * 3
+	_distance += delta * _speed * 2
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
--- src/attacks/weapons/shotgun/shotgun.gd
+++ src/attacks/weapons/shotgun/shotgun.gd
@@ -8,3 +8,28 @@ func init(parent_position: Node):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	super(delta)
+
+
+func true_shoot(angle : float):
+	var bullet: Fireball = _Fireball.instantiate()
+
+	nearest_enemy_pos = Context.get_nearest_enemy(position)
+	shoot_direction = (nearest_enemy_pos - position).normalized()
+	_wanted_theta = atan2(shoot_direction.y, shoot_direction.x)
+	
+	var shoot_angle: float = atan2(shoot_direction.y, shoot_direction.x)
+	
+	shoot_direction = Vector2(cos(angle + shoot_angle), sin(angle + shoot_angle)).normalized()
+
+
+	bullet.init(position, shoot_direction, 500)
+
+	bullets_node.add_child(bullet)
+
+func _on_timer_shots_timeout():
+	true_shoot(PI / 12)
+	true_shoot(-PI / 12)
+	true_shoot(2 * PI / 12)
+	true_shoot(- 2 * PI / 12)
+	true_shoot(0)
+	
