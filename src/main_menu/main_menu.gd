97793eede5e99256c1822c92ccf1030fca281971
Gatien Menaud <gatienmenaud@gmail.com>
2024-01-27 15:48:59 +0100

    quit and play

---
+++ src/main_menu/main_menu.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/AnimationPlayer.play("start")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://src/maps/first_map/map.tscn")
